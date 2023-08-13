8d9721f6e1bc2ed135e8f28dcc9a12a372fdd890
Nezca17 <126973834+Nezca17@users.noreply.github.com>
2023-08-13 16:14:48 -0600

    Prueba

--- PM2Ejercicio3_1/PM2Ejercicio3_1/Views/AlumnosView.xaml.cs
+++ PM2Ejercicio3_1/PM2Ejercicio3_1/Views/AlumnosView.xaml.cs
@@ -1,6 +1,10 @@
-﻿using PM2Ejercicio3_1.ViewModels;
+﻿using Firebase.Storage;
+using Plugin.Media.Abstractions;
+using Plugin.Media;
+using PM2Ejercicio3_1.ViewModels;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +19,7 @@ namespace PM2Ejercicio3_1.Views
     {
 
         private string photoPath;
+     //   private MediaFile photo;
         public AlumnosView()
         {
             InitializeComponent();
@@ -27,25 +32,85 @@ namespace PM2Ejercicio3_1.Views
         }
         private async void SeleccionarImagen_ClickedAsync(object sender, EventArgs e)
         {
+
+            /*
             try
             {
-                var photo = await MediaPicker.CapturePhotoAsync();
+                // Tomar una foto
+                photo = await CrossMedia.Current.TakePhotoAsync(new StoreCameraMediaOptions
+                {
+                    Directory = "CapturedPhotos",
+                    Name = "capturedImage.jpg",
+                    SaveToAlbum = true // Guardar la foto en el álbum de fotos del dispositivo (opcional)
+                });
 
                 if (photo != null)
                 {
-                    photoPath = photo.FullPath;
-                    ImagenPreview.Source = ImageSource.FromStream(() => photo.OpenReadAsync().Result);
+                    // Obtener la ruta de la foto capturada
+                    filePath = photo.Path;
+
+                    ImagenPreview.Source = ImageSource.FromFile(filePath);
+
                 }
+                var stream = photo.GetStream();
+                lbRutaImagenFile.Text = await TomarFoto(photo.)
+
             }
             catch (Exception ex)
             {
                 await DisplayAlert("Error", $"No se pudo tomar la foto: {ex.Message}", "OK");
             }
 
+            */
 
+        }
         private void btnVerLista_Clicked(object sender, EventArgs e)
         {
             Navigation.PushAsync(new ListaAlumnos());
+        }
+        public async Task<string> TomarFoto(Stream fotoFile, string nombre)
+        {
+            try
+            {
+
+                var photo = fotoFile;
+
+                if (photo != null)
+                {
+                    var task = new FirebaseStorage("pm2ejercicio3android.appspot.com", new FirebaseStorageOptions
+                    {
+
+                        ThrowOnCancel = true
+
+                    }).Child("Alumnos")
+                    .Child(NombresEntry.Text)
+                    .Child(nombre)
+                    .PutAsync(photo);
+
+                    task.Progress.ProgressChanged += (s, args) =>
+                    {
+                        progressBar.Progress = args.Percentage;
+                    };
+
+                    var dowloadlink = await task;
+
+                    return dowloadlink;
+                }
+                else
+                {
+                    return "No se envio";
+                }
+
+
+
+            }
+            catch (Exception e)
+            {
+
+
+                await App.Current.MainPage.DisplayAlert("Aviso", $"{e}", "Ok");
+                return "N/A";
+            }
         }
     }
 }
