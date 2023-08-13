07570671729515c93a5959fdbd8df681136d13f2
gereld10 <gereldamids42@gmail.com>
2023-08-13 13:41:51 -0600

    YAAAAAA

---
+++ PM2Ejercicio3_1/PM2Ejercicio3_1/Views/Alumnos.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PM2Ejercicio3_1.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Alumnos : ContentPage
+    {
+        public Alumnos()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
